aea15f820465f30f175fe1d9e7911e94e0092737
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-06 11:34:24 -0800

    add link to past events page

--- archive-event.php
+++ archive-event.php
@@ -48,8 +48,14 @@ get_header(); ?>
 
   <?php }
   echo paginate_links()
-
   ?>
+
+<hr class="section-break">
+
+    <p>Looking for a recap of our past events? 
+      <a href="<?php echo site_url('/past-events') ?>
+      ">Check out our past events archive</a>.
+    </p>
 
 </div>
 
