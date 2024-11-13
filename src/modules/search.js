659ff1aabac6316781215520fe1ba7a8ec916803
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-13 10:37:56 -0800

    Add search overlay when clicking search icon

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,19 +1,28 @@
+import $ from 'jquery';
+
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
-    this.openButton = " "
-
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.events();
   }
   // 2. events
-
+  events() {
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverly.bind(this));
+  }
 
   // 3. methods (functions, actions..)
   openOverlay() {
-
+    this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
   }
 
   closeOverly() {
-
+    this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
   }
 }
 
