b789db79fc8a5ac8a909af4c51ffdad7c381f49f
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-13 16:45:25 -0800

    add case to handle a search with no results

--- src/modules/search.js
+++ src/modules/search.js
@@ -47,8 +47,9 @@ class Search {
       this.searchField.val(), posts => {
         this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
-          <ul class="link-list min-list">
+          ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
             ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+            ${posts.length ? '</ul>' : ''}
           </ul>
         `);
       });
