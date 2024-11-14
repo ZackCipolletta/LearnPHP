f8231d17d127c979572404ed5ea55576a8aed59a
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-14 14:53:50 -0800

    WIP create a custom API route

--- inc/search-route.php
+++ inc/search-route.php
@@ -1,6 +1,18 @@
-<?php 
+<?php
 
-add_action('');
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch() {
+  // takes 3 arguments; 1 the name space, 2 the route (the ending part of the URL)
+  // 3 (associative) array that describes what should happen when someone visits the url
+  register_rest_route('university/v1', 'search', array(
+    'methods' => 'GET',
+    'callback' => 'universitySearchResults'
+  ));
+
+};
 
 
-?>
+function universitySearchResults () {
+  return 'Congratulations, you created a custom route.';
+}
