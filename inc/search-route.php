634e6cfe8b9e7fad910d13d0ff5685692b14a462
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-15 12:03:33 -0800

    WIP add content sorting to search results

--- inc/search-route.php
+++ inc/search-route.php
@@ -14,25 +14,66 @@ function universityRegisterSearch()
 
 function universitySearchResults($data)
 {
-  $professors = new WP_Query(array(
-    'post_type' => 'professor',
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
     // s stands for search
     's' => sanitize_text_field($data['term'])
   ));
 
-  $professorResults = array();
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  );
 
   // however many number of posts are in the collection of the 'professors' object is however
   // many times the loop should run
-  while ($professors->have_posts()) {
+  while ($mainQuery->have_posts()) {
     // this gets all the data from the relevant post ready and accessible
-    $professors->the_post();
-    // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink()
-    ));
-  }
+    $mainQuery->the_post();
 
-  return $professorResults;
+    if (get_post_type() === 'post' || get_post_type() === 'page') {
+      // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() === 'professor') {
+      // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() === 'program') {
+      // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() === 'campus') {
+      // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+
+      if (get_post_type() === 'event') {
+        // takes 2 arguments: 1 the array you want to add onto, 2 what you want to add on to the array
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+    }
+
+    return $results;
+  }
 }
