160fc4854f5e4c181af9adab5af60a09f77456d1
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-11-18 15:39:25 -0800

    WIP update JavaScriptless search results page, add styling and formatting for each post type. Also handle case where there are no results

--- page-search.php
+++ page-search.php
@@ -70,13 +70,8 @@ while (have_posts()) {
       <!-- by default a form will submit itself to it's own url. But my adding the action and a url
       it will ihnstead submit to the specified url page.
       By Echoing out the site URL and a '/' it will submit to the homepage url for hte WP installation -->
-      <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-        <label class="headline headline--medium" for="s">Perform a New Search:</label>
-        <div class="search-form-row">
-          <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
-          <input class="search-submit" type="submit" value="Search">
-        </div>
-      </form>
+      <?php get_search_form(); ?>
+
 
 
     </div>
